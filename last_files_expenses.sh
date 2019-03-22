616fabdf649d1d8d77557e46520c9a047f64387f
Alon Levy <alon@trellis.ag>
2019-03-22 22:04:22 +0200

    add summary util

---
+++ last_files_expenses.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function error() {
+	echo $@
+	exit -1
+}
+
+(which xsv 2> /dev/null) || error install rust and xsv
+
+xsv cat rows $(find . -maxdepth 1 -ctime -14 -iname 'fibi_last_month*.csv') | xsv sort | uniq \
+| xsv select expense,value_date $f | grep -v '^,' > last_expenses.csv
+xsv join --left expense last_expenses.csv  cost recurring_by_cost.csv  | xsv select expense,value_date,recurring > last_expenses_with_recurring.csv
+xsv join --left expense,value_date last_expenses_with_recurring.csv expense,value_date one_time.csv \
+  |  xsv select expense,value_date,recurring,one_time | xsv table
+
